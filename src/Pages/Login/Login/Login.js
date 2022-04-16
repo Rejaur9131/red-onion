7a45d9cc9e0823c1224d457f729d13f015720fc2
Rejaur9131 <raju_seu@yahoo.com>
2022-04-16 14:13:04 +0600

    Login function implemented

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import logo from '../../../images/logo2.png';
 import './Login.css';
+import auth from './../../../firebase.init';
 
 const Login = () => {
   const navigate = useNavigate();
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const location = useLocation();
+
+  let from = location.state?.from?.pathname || '/';
+
+  const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    signInWithEmailAndPassword(email, password);
+  };
+
   return (
     <div className="w-25 mx-auto py-5">
       <img src={logo} alt="" className="w-75 my-5" />
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter Email" required />
+          <Form.Control ref={emailRef} type="email" placeholder="Enter Email" required />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" required />
+          <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
         </Form.Group>
 
         <Button variant="danger" type="submit" className="w-100 ">
